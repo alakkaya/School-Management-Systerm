924949ee1a395c61110ae4ce4e37b9f70263054a
aliAkkayaJ <aliakkayaa0@gmail.com>
2023-12-24 14:28:56 +0300

    deleted all datas for each collections

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
+//delete all data's in collections to start again.
+const deleteAllData = async () => {
+  try {
+    // Takes all collectionNames
+    const modelNames = mongoose.modelNames();
+
+    // Delete all datas for each collection
+    for (const modelName of modelNames) {
+      const Model = mongoose.model(modelName);
+      await Model.deleteMany({});
+      console.log(`Veriler silindi: ${modelName}`);
+    }
+  } catch (error) {
+    console.error("The datas couldn't delete", error);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+//to delete all datas use deleteAllData()
+
+//connect the db
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
