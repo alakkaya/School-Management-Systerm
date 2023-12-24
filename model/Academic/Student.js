e7c0718f07a2eb9b958abb76d01e15919baa1964
aliAkkayaJ <aliakkayaa0@gmail.com>
2023-12-24 14:03:21 +0300

    u can enter class level like"level 100..."

--- model/Academic/Student.js
+++ model/Academic/Student.js
@@ -37,8 +37,7 @@ const studentSchema = new mongoose.Schema(
     //keep track of the class level the student is in...so first grades first floor ...ex.
     classLevels: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "ClassLevel",
+        type: String,
       },
     ],
     currentClassLevel: {
